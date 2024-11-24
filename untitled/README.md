1fb1ce60b5751f5de198c9b9c5babf21372bd394
CryptoManderOfficial <142843615+CryptoManderOfficial@users.noreply.github.com>
2024-11-24 13:18:27 -0600

    Add files via upload

---
+++ untitled/README.md
@@ -0,0 +1,4 @@
+# Untitled
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Perceptualitet/pen/azooZmo](https://codepen.io/Perceptualitet/pen/azooZmo).
+
---
+++ untitled/dist/script.js
@@ -0,0 +1,20 @@
+// Smooth scroll functionality
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToPlatform() {
+    document.getElementById('platform').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToEvents() {
+    document.getElementById('events').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToVolunteer() {
+    document.getElementById('volunteer').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToFooter() {
+    document.querySelector('footer').scrollIntoView({ behavior: 'smooth' });
+}
